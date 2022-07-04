8dbf76e7395804a416df7794f5eddec07005902a
Subrahmanya G T <subrahmanyagt@gmail.com>
2022-07-04 18:02:36 +0530

    add

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 2100
+const stripe = require("stripe")('sk_test_51JrsigAOaRtl8XJSVfUA8hcDAyvJ2LIuXHOu6eFrQ5eyp4GtV41CMVndxrBdiVm503PNZU3PjuxgksduUagoPXI400e3Kl0TWb');
+
+app.get('/create',  async(req, res) => {
+
+    const customer = await stripe.customers.create({
+       email:"exaple1@domain.com",
+      });
+      console.log(customer);
+  res.send({"customer": customer} )
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+// const stripe = require("stripe")('');
+
+// export default async function handler(req, res) {
+//   let userdetails = JSON.parse(req.body);
+  
+//   let custCheck = await stripe.customers.list({
+//     email: userdetails.email,
+//   });
+
+//   let data = { customer: null, errors: null };
+//   if (custCheck.data.length <= 0) {
+//     data.customer = await stripe.customers.create({
+//       name: userdetails.email,
+//       email: userdetails.email,
+//     });
+//   } else {
+//     data.errors = "Email" + userdetails.email + "already exists";
+//   }
+//   res.status(200).json({ data });
+// }
--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
+// const stripe = require("stripe")('sk_test_51JrsigAOaRtl8XJSVfUA8hcDAyvJ2LIuXHOu6eFrQ5eyp4GtV41CMVndxrBdiVm503PNZU3PjuxgksduUagoPXI400e3Kl0TWb');
+
+const stripeFunction= async()=>{
+  const response = await fetch('/create');
+    const body =  response;
+    console.log(body);
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={stripeFunction}>
+        example button
+     </button>
     </div>
   );
 }
